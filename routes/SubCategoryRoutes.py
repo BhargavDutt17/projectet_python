848bc23fe43f6d25d57b260a8aff2acdcae036e3
BhargavDutt17 <duttbhargav17@gmail.com>
2025-03-20 11:27:01 +0530

    sub category routes are now visible in swagger ui

--- routes/SubCategoryRoutes.py
+++ routes/SubCategoryRoutes.py
@@ -1,20 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from models.SubCategoryModel import SubCategory
 from controllers import SubCategoryController
 
 router = APIRouter()
+
+
 @router.post("/addSubCategory")
-async def post_sub_category(sub_cat:SubCategory):
+async def post_sub_category(sub_cat: SubCategory):
     return await SubCategoryController.addSubCategory(sub_cat)
+
 
 @router.get("/getAllSubCategories")
 async def get_all_sub_categories():
     return await SubCategoryController.getAllSubCategories()
 
-from fastapi import APIRouter, Query
-from controllers import SubCategoryController
-
-router = APIRouter()
 
 @router.get("/getSubCategoryByCategoryId/{category_id}")
 async def get_sub_category_by_category_id(
@@ -22,4 +21,6 @@ async def get_sub_category_by_category_id(
     user_id: str = Query(None, description="Logged-in user's ID"),
     role_id: str = Query(None, description="User's role ID"),
 ):
-    return await SubCategoryController.getSubCategoryByCategoryId(category_id, user_id, role_id)
+    return await SubCategoryController.getSubCategoryByCategoryId(
+        category_id, user_id, role_id
+    )
